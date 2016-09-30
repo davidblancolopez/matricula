5af072218e38004387482689fc76eb6bfe88d411
davidblancolopez <daviddbl94@gmail.com>
2016-09-30 19:32:04 +0200

    Merge commit '8d6d050d5c604f1ce8350b5ceb1f09a4e2516a62'
    Cambio matricula

---
+++ src/matricula/Matricula.java
@@ -0,0 +1,28 @@
+
+package matricula;
+
+import java.util.Scanner;
+
+
+
+public class Matricula {
+
+    
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        int numerouf;
+        
+        do{
+        System.out.println("Introdueix el numero de UF's en que estas matriculat: ");
+        numerouf = lector.nextInt();
+        }while(numerouf > 0);
+        
+        if (numerouf > 12) {
+            System.out.println("El preu total és de: " + (numerouf * 30));
+        }else if (numerouf >= 12){
+            System.out.println("El preu total és de: 380,00 €");
+        }
+        
+    }
+    
+}
