07fd1332d233ecff67825e8cfea36f011f7a860e
David <David@CHRONOS>
2016-10-05 13:26:28 +0200

    Resueltos < puestos al reves en el codigo.

--- src/matricula/Matricula.java
+++ src/matricula/Matricula.java
@@ -15,9 +15,10 @@ public class Matricula {
         do{
         System.out.println("Introdueix el numero de UF's en que estas matriculat: ");
         numerouf = lector.nextInt();
-        }while(numerouf > 0);
         
-        if (numerouf > 12) {
+        }while(numerouf < 0);
+        
+        if (numerouf < 12) {
             System.out.println("El preu total és de: " + (numerouf * 30));
         }else if (numerouf >= 12){
             System.out.println("El preu total és de: 380,00 €");
